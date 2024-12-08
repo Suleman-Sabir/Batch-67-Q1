26b1ae97c21dc85f4e83aada3513b0a2735adb8d
Jahanzaib Tayyab <jtayyab204@gmail.com>
2024-12-08 18:32:14 +0500

    run first python program

---
+++ A1-101/8-Dec/helloworld.py
@@ -0,0 +1,12 @@
+# The `print` statement in Python is a built-in function used to display output on the screen. It takes one or more items, converts them into a string if needed, and shows them in the console or standard output. For example:
+
+# ```python
+# print("Hello, World!")
+# ```
+
+# This outputs:  
+# **Hello, World!**
+
+# It is commonly used to show messages, display variable values, and debug programs.
+
+print("Hello, World! This is my first python program")
