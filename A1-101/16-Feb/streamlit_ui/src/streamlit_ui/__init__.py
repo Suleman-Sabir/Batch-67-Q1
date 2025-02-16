a17a9a081a4ff02b5307b6e6a57cace6bbbd67b0
Jahanzaib Tayyab <jtayyab204@gmail.com>
2025-02-16 17:34:26 +0500

    create basic project with UV

---
+++ A1-101/16-Feb/streamlit_ui/src/streamlit_ui/__init__.py
@@ -0,0 +1,4 @@
+import streamlit as st # type: ignore
+
+def main() -> None:
+    st.write('Hello From Streamlit')
---
+++ A1-101/16-Feb/streamlit_ui/src/streamlit_ui/main.py
@@ -0,0 +1,9 @@
+import streamlit as st # type: ignore
+
+
+st.write('Hello From Streamlit')
+st.header("Welcome to my app!")
+# Subheader
+st.subheader("This is a demo of Streamlit")
+# Text
+st.write("Streamlit is great for building interactive web applications.")
