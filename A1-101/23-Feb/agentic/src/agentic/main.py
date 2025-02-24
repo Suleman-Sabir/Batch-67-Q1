b31a27271e05cef8dc217dcc75270bd185ac73ff
Kamran-it009 <kamran.it009@gmail.com>
2025-02-24 13:53:04 +0500

    deleted

--- A1-101/23-Feb/agentic/src/agentic/main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Agentic AI")
-st.text("This is a PIAIC Batch-67. We are learning Agentic AI.")
-st.write("This is a PIAIC Batch-67. We are learning Agentic AI.")
-
-st.video("https://www.youtube.com/watch?v=Avgum_r9i8E")
-
--- A1-101/23-Feb/agentic/src/agentic/script.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from streamlit.web import cli
-
-def app():
-    sys.argv = ['streamlit', 'run', 'src/agentic/main.py']
-    cli.main()
-
