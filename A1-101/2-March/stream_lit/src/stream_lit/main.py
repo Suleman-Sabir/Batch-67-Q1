ba37dd4f29508a9db060b3f4cd2f736ae8580b3a
Jahanzaib Tayyab <jtayyab204@gmail.com>
2025-03-02 21:09:25 +0500

    update project streamlit

---
+++ A1-101/2-March/stream_lit/src/stream_lit/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("This is 2nd March Header File")
+st.text("Write text without Markdown or HTML parsing.")
+st.text_area("Display a multi-line text input widget.")
+st.text_input("Display a single-line text input widget.")
